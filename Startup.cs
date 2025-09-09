aa32162ca232a4292818a4884a542af7bb995ad0
jfhflores0711 <jfh.flores.11@gmail.com>
2025-09-09 14:55:47 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+[assembly: OwinStartup(typeof(OpenB.Startup))]
+namespace OpenB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
